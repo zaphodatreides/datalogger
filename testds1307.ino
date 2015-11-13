d1a2b1b2c99465102ff4667261ead68de815f075
zaphodatreides <v.atreides2010@gmail.com>
2015-11-13 11:31:06 +0000

    loop: reading time

--- testds1307.ino
+++ testds1307.ino
@@ -18,6 +18,15 @@ unsigned char i;
       Serial.print(" ");
     } 
   } else Serial.println ("Error reading all registers");
+    Wire.requestFrom(DS1307_CTRL_ID, NBRREGS);
+  if (Wire.available() >= NBRREGS) {
+    for (i=0;i<NBRREGS;i++){
+      Serial.print (Wire.read());
+      Serial.print(" ");
+    } 
+  } else Serial.println ("Error reading all registers");
+
+
   } else Serial.println ("Error Detecting ds1307");
   Serial.println("");
   Serial.println ("Writing ram registers");
@@ -25,7 +34,7 @@ unsigned char i;
 
   Wire.beginTransmission(DS1307_CTRL_ID);
   Wire.write((uint8_t)0x08); // set ptr to ram
-  for (i=0x08;i<NBRREGS;i++) Wire.write(i);
+  for (i=0x08;i<64;i++) Wire.write(i);
   if (Wire.endTransmission() != 0) {
     Serial.println ("No ds1307...");
   }
@@ -41,7 +50,34 @@ unsigned char i;
       Serial.print (" ");
     } 
   } else Serial.println ("Error reading all registers");
+      Wire.requestFrom(DS1307_CTRL_ID, NBRREGS);
+  if (Wire.available() >= NBRREGS) {
+    for (i=0;i<NBRREGS;i++){
+      Serial.print (Wire.read());
+      Serial.print (" ");
+    } 
+  } else Serial.println ("Error reading all registers");
+
 }  
+  Wire.beginTransmission(DS1307_CTRL_ID);
+  Wire.write((uint8_t)0x00); // set ptr to ram
+  Wire.write((uint8_t)0x00);
+  Wire.endTransmission();
+
 }
 void loop () {
+  unsigned char i;
+     Wire.beginTransmission(DS1307_CTRL_ID);
+   Wire.write((uint8_t)0x00); 
+  if (Wire.endTransmission() == 0) {
+  Wire.requestFrom(DS1307_CTRL_ID, 8);
+  if (Wire.available() >= 8) {
+    for (i=0;i<8;i++){
+      Serial.print (Wire.read());
+      Serial.print(" ");
+    }
+    Serial.println("");
+  }
+  }
+  
 }
